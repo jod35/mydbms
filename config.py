05685d546961cb9ec4f10c510f9e349e370826b5
jod35 <jodestrevin@gmail.com>
2019-09-25 09:51:40 -0400

    venv added to base project

---
+++ config.py
@@ -0,0 +1,6 @@
+class DevConfig:
+    SQLALCHEMY_DATABASE_URI='mysql://jon:nathanoj35@localhost/dbms'
+    SQLALCHEMY_ECHO=True
+    SQLCHEMY_TRACK_MODIFICATIONS=False
+    SECRET_KEY='$@$@42K3J4K23J423O4923I4923523%$#%#$%2'
+    
